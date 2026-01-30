052abb569b86162d88896cc032d4e8ee9beec890
ilua2013 <ilua.2013@inbox.ru>
2026-01-30 17:23:20 +0300

    Практическое применение PageFactory в автотестах

--- components/charts/chart_view_component.py
+++ components/charts/chart_view_component.py
@@ -1,15 +1,17 @@
-from typing import Literal
 from components.base_component import BaseComponent
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
+
+from elements.image import Image
+from elements.text import Text
 
 class ChartViewComponent(BaseComponent):
     def __init__(self, page: Page, identifier: str, chart_type: str):
         super().__init__(page)
-        self.title = page.get_by_test_id(f'{identifier}-widget-title-text')
-        self.chart = page.get_by_test_id(f'{identifier}-{chart_type}-chart')
+        self.title = Text(page, f'{identifier}-widget-title-text', 'title')
+        self.chart = Image(page, f'{identifier}-{chart_type}-chart', 'Chart')
 
     def check_visible(self, title: str):
-        expect(self.title).to_be_visible()
-        expect(self.title).to_have_text(title)
-        expect(self.chart).to_be_visible()
+        self.title.check_visible()
+        self.title.check_have_text(title)
+        self.chart.check_visible()
 
